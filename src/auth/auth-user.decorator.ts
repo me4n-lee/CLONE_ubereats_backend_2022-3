c7931715295ec58a682656ac71e5f440cedf32c3
keyleeu <minhk.lee21@gmail.com>
2022-04-01 22:30:54 +0900

    #end

---
+++ src/auth/auth-user.decorator.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+
+export const AuthUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const gqlContext = GqlExecutionContext.create(context).getContext();
+    const user = gqlContext['user'];
+    return user;
+  },
+);
---
+++ src/auth/role.decorator.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+
+import { SetMetadata } from '@nestjs/common';
+import { UserRole } from 'src/users/entities/user.entity';
+
+export type AllowedRoles = keyof typeof UserRole | 'Any';
+
+export const Role = (roles: AllowedRoles[]) => SetMetadata('roles', roles);
